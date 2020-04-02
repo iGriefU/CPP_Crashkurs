2068eec39be142db5b8e4a4c9cea8b674355d47b
Max <j.erricson@gmx.de>
2020-04-02 11:01:38 +0200

    shape downgrade fix

--- spielereien/Shape.h
+++ spielereien/Shape.h
@@ -1,46 +1,10 @@
 #pragma once
-#include <iostream>
-#include <string>
+
 class Shape
 {
-
 public:
-    Shape()
+     virtual unsigned int corners()
     {
-        assignEckig();
-        this->name = "Abstrakte Form";
-        ecken = 0;
-    }
-
-    Shape(std::string name)
-    {
-        assignEckig();
-        this->name = name;
-        ecken = 0;
-    }
-    unsigned int corners()
-    {
-        return ecken;
-    }
-    void draw()
-    {
-        std::cout << name << " hat " << ecken << " Ecken." << std::endl;
-    }
-
-private:
-    unsigned int ecken;
-    bool eckig;
-    std::string name;
-    void assignEckig()
-    {
-        if (ecken)
-        {
-            eckig = false;
-        }
-        else
-        {
-            eckig = true;
-        }
-        this->name = name;
+        return 0;
     }
 };
--- spielereien/vererbung.cpp
+++ spielereien/vererbung.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include "Shape.h"
 
+class Triangle : public Shape
+{
+public:
+    unsigned int corners() override
+    {
+        return 3;
+    }
+};
+
+void draw(Shape &s)
+{
+    std::cout << s.corners() << " Ecken" << std::endl;
+}
+
 int main()
 {
-    Shape *form = new Shape();
-    form->draw();
+    Triangle form;
+    draw(form);
     return 0;
 }
 
